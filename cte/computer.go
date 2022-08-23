c443417f3af535aaa157eb4051b67d6391b0038e
James Tran <james.tran@grabtaxi.com>
2022-08-23 14:19:24 +0700

    rename struct

--- cte/computer.go
+++ cte/computer.go
@@ -8,30 +8,30 @@ import (
 )
 
 type ImpureComputer interface {
+	Compute(ctx context.Context, p MasterPlan) (any, error)
+}
+
+type ImpureComputerWithLoadingData interface {
 	LoadingComputer
 	Compute(ctx context.Context, p MasterPlan, data LoadingData) (any, error)
 }
 
-type ImpureComputerWithoutLoadingData interface {
-	Compute(ctx context.Context, p MasterPlan) (any, error)
+type SideEffectComputer interface {
+	Compute(ctx context.Context, p MasterPlan) error
 }
 
-type SideEffectComputer interface {
+type SideEffectComputerWithLoadingData interface {
 	LoadingComputer
 	Compute(ctx context.Context, p MasterPlan, data LoadingData) error
 }
 
-type SideEffectComputerWithoutLoadingData interface {
-	Compute(ctx context.Context, p MasterPlan) error
+type SwitchComputer interface {
+	Switch(ctx context.Context, p MasterPlan) (MasterPlan, error)
 }
 
-type SwitchComputer interface {
+type SwitchComputerWithLoadingData interface {
 	LoadingComputer
 	Switch(ctx context.Context, p MasterPlan, data LoadingData) (MasterPlan, error)
-}
-
-type SwitchComputerWithoutLoadingData interface {
-	Switch(ctx context.Context, p MasterPlan) (MasterPlan, error)
 }
 
 type LoadingComputer interface {
@@ -53,41 +53,41 @@ type computerWrapper struct {
 	LoadingComputer
 
 	ImpureComputer
-	ImpureComputerWithoutLoadingData
+	ImpureComputerWithLoadingData
 	SideEffectComputer
-	SideEffectComputerWithoutLoadingData
+	SideEffectComputerWithLoadingData
 	SwitchComputer
-	SwitchComputerWithoutLoadingData
+	SwitchComputerWithLoadingData
 }
 
 func newComputerWrapper(rawComputer any) computerWrapper {
 	switch c := rawComputer.(type) {
+	case ImpureComputerWithLoadingData:
+		return computerWrapper{
+			LoadingComputer:               c,
+			ImpureComputerWithLoadingData: c,
+		}
 	case ImpureComputer:
 		return computerWrapper{
-			LoadingComputer: c,
-			ImpureComputer:  c,
+			ImpureComputer: c,
 		}
-	case ImpureComputerWithoutLoadingData:
+	case SideEffectComputerWithLoadingData:
 		return computerWrapper{
-			ImpureComputerWithoutLoadingData: c,
+			LoadingComputer:                   c,
+			SideEffectComputerWithLoadingData: c,
 		}
 	case SideEffectComputer:
 		return computerWrapper{
-			LoadingComputer:    c,
 			SideEffectComputer: c,
 		}
-	case SideEffectComputerWithoutLoadingData:
+	case SwitchComputerWithLoadingData:
 		return computerWrapper{
-			SideEffectComputerWithoutLoadingData: c,
+			LoadingComputer:               c,
+			SwitchComputerWithLoadingData: c,
 		}
 	case SwitchComputer:
 		return computerWrapper{
-			LoadingComputer: c,
-			SwitchComputer:  c,
-		}
-	case SwitchComputerWithoutLoadingData:
-		return computerWrapper{
-			SwitchComputerWithoutLoadingData: c,
+			SwitchComputer: c,
 		}
 	default:
 		panic(ErrInvalidComputerType.Err(reflect.TypeOf(c)))
@@ -95,24 +95,24 @@ func newComputerWrapper(rawComputer any) computerWrapper {
 }
 
 func (w computerWrapper) Compute(ctx context.Context, p MasterPlan, data LoadingData) (any, error) {
-	if w.ImpureComputerWithoutLoadingData != nil {
-		return w.ImpureComputerWithoutLoadingData.Compute(ctx, p)
+	if w.ImpureComputer != nil {
+		return w.ImpureComputer.Compute(ctx, p)
+	}
+
+	if w.SideEffectComputerWithLoadingData != nil {
+		return struct{}{}, w.SideEffectComputerWithLoadingData.Compute(ctx, p, data)
 	}
 
 	if w.SideEffectComputer != nil {
-		return struct{}{}, w.SideEffectComputer.Compute(ctx, p, data)
-	}
-
-	if w.SideEffectComputerWithoutLoadingData != nil {
-		return struct{}{}, w.SideEffectComputerWithoutLoadingData.Compute(ctx, p)
+		return struct{}{}, w.SideEffectComputer.Compute(ctx, p)
 	}
 
 	mp, err := func() (MasterPlan, error) {
-		if w.SwitchComputer != nil {
-			return w.SwitchComputer.Switch(ctx, p, data)
+		if w.SwitchComputerWithLoadingData != nil {
+			return w.SwitchComputerWithLoadingData.Switch(ctx, p, data)
 		}
 
-		return w.SwitchComputerWithoutLoadingData.Switch(ctx, p)
+		return w.SwitchComputer.Switch(ctx, p)
 	}()
 
 	return toExecutePlan{
@@ -125,51 +125,51 @@ type SideEffect struct{}
 type SyncSideEffect struct{}
 
 type Result struct {
-    Task async.Task[any]
+	Task async.Task[any]
 }
 
 func newResult(t async.Task[any]) Result {
-    return Result{
-        Task: t,
-    }
+	return Result{
+		Task: t,
+	}
 }
 
 func Outcome[V any](t async.Task[any]) V {
-    if t == nil {
-        var tmp V
-        return tmp
-    }
+	if t == nil {
+		var tmp V
+		return tmp
+	}
 
-    // We only need to take care of situations where clients return "nil, err"
-    // in their computers. Other than that, if clients return a non-nil value,
-    // they must make sure the return type is as expected.
-    outcome, _ := t.Outcome()
-    if outcome != nil {
-        return outcome.(V)
-    }
+	// We only need to take care of situations where clients return "nil, err"
+	// in their computers. Other than that, if clients return a non-nil value,
+	// they must make sure the return type is as expected.
+	outcome, _ := t.Outcome()
+	if outcome != nil {
+		return outcome.(V)
+	}
 
-    var tmp V
-    return tmp
+	var tmp V
+	return tmp
 }
 
 type SyncResult struct {
-    Outcome any
+	Outcome any
 }
 
 func newSyncResult(o any) SyncResult {
-    return SyncResult{
-        Outcome: o,
-    }
+	return SyncResult{
+		Outcome: o,
+	}
 }
 
 func Cast[V any](o any) V {
-    // We only need to take care of situations where clients return "nil, err"
-    // in their computers. Other than that, if clients return a non-nil value,
-    // they must make sure the return type is as expected.
-    if o != nil {
-        return o.(V)
-    }
+	// We only need to take care of situations where clients return "nil, err"
+	// in their computers. Other than that, if clients return a non-nil value,
+	// they must make sure the return type is as expected.
+	if o != nil {
+		return o.(V)
+	}
 
-    var tmp V
-    return tmp
+	var tmp V
+	return tmp
 }
